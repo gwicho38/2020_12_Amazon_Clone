7e762d36c8b27a0361e88da914a4ad872df803cd
Luis Fernandez de la Vara <lef16b@my.fsu.edu>
2020-12-23 10:18:23 -0500

    Initial commit for home page branch.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0OxrsM2raizbwlOW5ikHY2L7B6uNa-v8",
+  authDomain: "challenge-7f24d.firebaseapp.com",
+  projectId: "challenge-7f24d",
+  storageBucket: "challenge-7f24d.appspot.com",
+  messagingSenderId: "457860344826",
+  appId: "1:457860344826:web:90c05980ed2a9fe4b4d243",
+  measurementId: "G-EPB2MGEC7K"
+};
