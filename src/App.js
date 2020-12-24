09261a82b3303e89e3eabb839377cbe3c21e7701
Luis E. Fernandez de la Vara <lef16b@my.fsu.edu>
2020-12-24 10:46:23 -0500

    Initial header complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
+    //BEM convention followed by naming app here
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Header Component -- top part of app*/}
+      <Header></Header>
+      {/*Home Component -- body of app*/}
     </div>
   );
 }
