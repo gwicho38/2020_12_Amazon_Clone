c6916a28ed16a4193f0f631b838014c7d49dda22
Luis E. Fernandez de la Vara <lef16b@my.fsu.edu>
2020-12-29 10:04:54 -0500

    Implemented firebase login functionality.

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC0OxrsM2raizbwlOW5ikHY2L7B6uNa-v8",
   authDomain: "challenge-7f24d.firebaseapp.com",
@@ -8,3 +10,9 @@ const firebaseConfig = {
   appId: "1:457860344826:web:90c05980ed2a9fe4b4d243",
   measurementId: "G-EPB2MGEC7K"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore(); 
+const auth = firebase.auth(); 
+
+export {db, auth};
