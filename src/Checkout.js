fbf948c5f6142db0c515e238ce75bbd181182b10
Luis E. Fernandez de la Vara <lef16b@my.fsu.edu>
2020-12-30 08:48:42 -0500

    Completed authentication logic.

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,7 +6,7 @@ import Subtotal from './Subtotal'
 
 function Checkout() {
 
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user}, dispatch] = useStateValue();
 
     return (
 
@@ -15,6 +15,7 @@ function Checkout() {
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
 
                 <div>
+                    <h3>Hello, {user ? user?.email : "Guest"}</h3>
                     <h2 className="checkout__title">Your shopping Basket</h2>
                     
                     {basket.map(item => (
